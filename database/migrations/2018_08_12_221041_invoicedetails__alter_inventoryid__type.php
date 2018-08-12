667caf82bfad5a971b63e93485e7ec13cb0eaba7
Calvin Choe <Calvinchoe1@gmail.com>
2018-08-12 18:21:02 -0400

    changing inventory_id to be nullable on InvoiceDetails

---
+++ database/migrations/2018_08_12_221041_invoicedetails__alter_inventoryid__type.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class InvoicedetailsAlterInventoryidType extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::table('InvoiceDetails', function(Blueprint $table) {
+            $table->integer('inventory_id')->unsigned()->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::table('InvoiceDetails', function(Blueprint $table) {
+            $table->integer('inventory_id')->unsigned()->change();
+        });
+    }
+}
