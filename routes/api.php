4e7bbd6594644dd140b4caa8ef02bd5fe14c8ecd
Calvin Choe <Calvinchoe1@gmail.com>
2018-08-12 22:51:10 -0400

    adding first api endpoint for customers

--- routes/api.php
+++ routes/api.php
@@ -15,4 +15,7 @@ use Illuminate\Http\Request;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+
 });
+Route::get('/customer/get/{id}', 'CustomerController@getCustomer');
+Route::post('/customer/post', 'CustomerController@postCustomer');
