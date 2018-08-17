4149c8c979d856b19cb81c37c00adc012114e778
Calvin Choe <Calvinchoe1@gmail.com>
2018-08-16 21:43:13 -0400

    setting up laravel mix to precompile sass

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -18,7 +18,8 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     
-    <link rel="stylesheet" type="text/css" href="/css/main.css">
+    <!-- <link rel="stylesheet" type="text/css" href="/css/main.css"> -->
+    <link rel="stylesheet" type="text/css" href="/css/app.css">
 
 </head>
 
@@ -28,7 +29,7 @@
             <p>{{ Session::get('message') }}</p>
         </div>
     @endif
-    <div id="container" class="container">
+    <div id="container">
         {{-- @include('partials._company') --}}
         @include('partials._nav')
         <h1 id="title">@yield('title')</h1>
--- webpack.mix.js
+++ webpack.mix.js
@@ -14,4 +14,6 @@ let mix = require('laravel-mix');
 /*mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css');*/
 
+mix.sass('resources/assets/sass/app.scss', 'public/css');
+
 mix.browserSync('http://localhost:8000');
